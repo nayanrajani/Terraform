4f186f76f1b3a062cde2381855f8ecc3fe74a4fb
nayanrajani <nayanrajani86@gmail.com>
2022-03-24 12:19:13 +0530

    Commit

--- Cloudflare-Terraform/main.tf
+++ Cloudflare-Terraform/main.tf
@@ -8,12 +8,8 @@ terraform {
 }
 
 provider "cloudflare" {
-  email     = "nayan.rajani@blazeclan.com"
-  api_token = "O_eqbwu3j7w-XcjB0AoG_oUHmXi9rRnuDyP7Mddq"
-}
-
-variable "zone_id" {
-  default = "0d32a989ca0e8d5e9155163f9f937d2b"
+  email     = var.email
+  api_token = var.token
 }
 
 variable "domain" {
@@ -29,39 +25,3 @@ resource "cloudflare_record" "DNS_entry" {
   type    = "A"
   proxied = true //chanage to true always
 }
-
-/*
-//Page rule
-// terraform destroy -target cloudflare_page_rule.Page_rule
-resource "cloudflare_page_rule" "Page_rule" {
-  zone_id  = var.zone_id
-  target   = "https://www.greencoin.cf/app/ok/now/*"
-  priority = 1
-  status   = "active" //active/disabled
-
-
-  actions {
-    cache_key_fields {
-      cookie {
-        check_presence = ["wordpress_test_cookie"]
-      }
-      header {
-        check_presence = ["header_present"]
-        exclude        = ["origin"]
-        include        = ["api-key", "dnt"]
-      }
-      host {
-        resolved = true
-      }
-      query_string {
-        ignore = true
-      }
-      user {
-        device_type = false
-        geo         = true
-        lang        = true
-      }
-    }
-  }
-}
-*/
--- Cloudflare-Terraform/variable.tf
+++ Cloudflare-Terraform/variable.tf
@@ -2,26 +2,26 @@ variable "zone_id" {
   description = "Please enter Zone ID"
   type        = string
   sensitive = true
-  default = "0d32a989ca0e8d5e9155163f9f937d2b"  //greencoin zone id
+  default = "<Zone ID>"  //zone id
 }
 
 variable "account_id" {
   description = "Please enter Account ID"
   type        = string
   sensitive = true
-  default = "63aa4349b5d9134c55d699ef7db2a636"  //greencoin zone id
+  default = "<Account ID>"  //Account id
 }
 
 variable "token" {
   description = "Please enter dns"
   type        = string
   sensitive   = true
-  default     = "O_eqbwu3j7w-XcjB0AoG_oUHmXi9rRnuDyP7Mddq"  //Common Token
+  default     = "<Token>"  //Common Token
 }
 variable "email" {
   description = "Please enter email"
   type        = string
   sensitive   = true
-  default     = "nayan.rajani@blazeclan.com"
+  default     = "<Email>"
 }
 
